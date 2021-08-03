8d09676924a90742947e650c4331ad75d26d2170
fletchthefletch <fletcher@4tradeit.co.nz>
2021-08-03 17:00:21 +1200

    Ready for save feature

--- src/main/java/nz/ac/massey/cs/texteditor/Listener.java
+++ src/main/java/nz/ac/massey/cs/texteditor/Listener.java
@@ -8,7 +8,12 @@ public class Listener implements ActionListener {
 		System.out.println(e.getActionCommand());
 		switch (e.getActionCommand()) {
 			case "New":
+				// 1. Check if the current file is saved
+				// 		Saved 	--> Create a new file
+				//		Unsaved 	--> Force user to save file 
+				
 				// Load a new file into the current window
+				
 			case "New Window":
 				// Load a new text editor instance
 				new TextEditor();
