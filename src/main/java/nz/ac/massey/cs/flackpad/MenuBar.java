0b1f34c5c3b4ccb0e7ab65b2b30b2ae68a607013
Jack Sandford <jacksandford5757@gmail.com>
2021-08-13 00:28:57 +1200

    Removed lists of menu items so they can be accessed

--- src/main/java/nz/ac/massey/cs/flackpad/MenuBar.java
+++ src/main/java/nz/ac/massey/cs/flackpad/MenuBar.java
@@ -2,7 +2,6 @@ package nz.ac.massey.cs.flackpad;
 
 import java.awt.Color;
 import java.awt.event.*;
-import java.util.LinkedHashMap;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -30,43 +29,74 @@ class MenuBar extends JMenuBar {
 	private JLabel filetype;
 	private JLabel zoomlevel;
 
+	// File menu items
+	private JMenuItem newItem;
+	private JMenuItem newWindowItem;
+	private JMenuItem openItem;
+	private JMenuItem saveItem;
+	private JMenuItem saveAsItem;
+	private JMenuItem printItem;
+	private JMenuItem pdfItem;
+	private JMenuItem exitItem;
+
+	// Edit menu items
+	private JMenuItem undoItem;
+	private JMenuItem redoItem;
+	private JMenuItem cutItem;
+	private JMenuItem copyItem;
+	private JMenuItem pasteItem;
+	private JMenuItem selectAllItem;
+	private JMenuItem deleteItem;
+	private JMenuItem findItem;
+	private JMenuItem tDItem;
+
+	// View menu items
+	private JMenuItem zoomInItem;
+	private JMenuItem zoomOutItem;
+	private JMenuItem resetZoomItem;
+	private JMenuItem lineNumbersItem;
+	private JMenuItem themeItem;
+
+	// Help menu items
+	private JMenuItem aboutItem;
+
 	MenuBar(Window window) {
 		super();
 
 		menuListener = new MenuListener(window);
 		this.window = window;
 
-		this.addFileMenu();
-		this.addEditMenu();
-		this.addViewMenu();
-		this.addHelpMenu();
-		this.addFindBar();
-		this.addInformationBar();
-		this.setBackground(menuBackground);
+		addFileMenu();
+		addEditMenu();
+		addViewMenu();
+		addHelpMenu();
+		addFindBar();
+		addInformationBar();
+		setBackground(menuBackground);
 	}
 
 	private void addInformationBar() {
 		// Char count
 		details = new JLabel("0 | Char");
 		details.setBorder(
-				BorderFactory.createCompoundBorder(this.getBorder(), BorderFactory.createEmptyBorder(0, 0, 0, 10)));
-		this.add(Box.createHorizontalGlue());
+				BorderFactory.createCompoundBorder(getBorder(), BorderFactory.createEmptyBorder(0, 0, 0, 10)));
+		add(Box.createHorizontalGlue());
 		details.setForeground(Color.decode("#990000"));
-		this.add(details);
+		add(details);
 
 		// Zoom percentage
 		zoomlevel = new JLabel();
 		zoomlevel.setBorder(
-				BorderFactory.createCompoundBorder(this.getBorder(), BorderFactory.createEmptyBorder(0, 0, 0, 10)));
+				BorderFactory.createCompoundBorder(getBorder(), BorderFactory.createEmptyBorder(0, 0, 0, 10)));
 		zoomlevel.setForeground(Color.decode("#444444"));
-		this.add(zoomlevel);
+		add(zoomlevel);
 
 		// File type label
 		filetype = new JLabel();
 		filetype.setBorder(
-				BorderFactory.createCompoundBorder(this.getBorder(), BorderFactory.createEmptyBorder(0, 0, 0, 10)));
+				BorderFactory.createCompoundBorder(getBorder(), BorderFactory.createEmptyBorder(0, 0, 0, 10)));
 		filetype.setForeground(Color.decode("#777777"));
-		this.add(filetype);
+		add(filetype);
 	}
 
 	public void setInformationBarText(String val) {
@@ -94,27 +124,46 @@ class MenuBar extends JMenuBar {
 		fileMenu.setForeground(menuItemTabForeground);
 		fileMenu.setMnemonic(KeyEvent.VK_F);
 
-		LinkedHashMap<String, KeyStroke> fileItems = new LinkedHashMap<String, KeyStroke>();
-		fileItems.put("New", KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
-		fileItems.put("New Window",
-				KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK + InputEvent.SHIFT_DOWN_MASK));
-		fileItems.put("Open", KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
-		fileItems.put("Save", KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
-		fileItems.put("Save As",
-				KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK + InputEvent.SHIFT_DOWN_MASK));
-		fileItems.put("Print", KeyStroke.getKeyStroke(KeyEvent.VK_P, InputEvent.CTRL_DOWN_MASK));
-		fileItems.put("Export to PDF", null);
-		fileItems.put("Exit", KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_DOWN_MASK));
+		newItem = new JMenuItem("New");
+		newItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+		newItem.addActionListener(menuListener);
+		fileMenu.add(newItem);
 
-		for (String itemName : fileItems.keySet()) {
-			JMenuItem item = new JMenuItem(itemName);
-			if (fileItems.get(itemName) != null)
-				item.setAccelerator(fileItems.get(itemName));
-			item.addActionListener(menuListener);
-			fileMenu.add(item);
-		}
+		newWindowItem = new JMenuItem("New Window");
+		newWindowItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK));
+		newWindowItem.addActionListener(menuListener);
+		fileMenu.add(newWindowItem);
 
-		this.add(fileMenu);
+		openItem = new JMenuItem("Open");
+		openItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
+		openItem.addActionListener(menuListener);
+		fileMenu.add(openItem);
+
+		saveItem = new JMenuItem("Save");
+		saveItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+		saveItem.addActionListener(menuListener);
+		fileMenu.add(saveItem);
+
+		saveAsItem = new JMenuItem("Save As");
+		saveAsItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK));
+		saveAsItem.addActionListener(menuListener);
+		fileMenu.add(saveAsItem);
+
+		printItem = new JMenuItem("Print");
+		printItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, InputEvent.CTRL_DOWN_MASK));
+		printItem.addActionListener(menuListener);
+		fileMenu.add(printItem);
+
+		pdfItem = new JMenuItem("Export to PDF");
+		pdfItem.addActionListener(menuListener);
+		fileMenu.add(pdfItem);
+
+		exitItem = new JMenuItem("Exit");
+		exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, InputEvent.CTRL_DOWN_MASK));
+		exitItem.addActionListener(menuListener);
+		fileMenu.add(exitItem);
+
+		add(fileMenu);
 	}
 
 	private void addEditMenu() {
@@ -122,25 +171,52 @@ class MenuBar extends JMenuBar {
 		editMenu.setForeground(menuItemTabForeground);
 		editMenu.setMnemonic(KeyEvent.VK_E);
 
-		LinkedHashMap<String, KeyStroke> editItems = new LinkedHashMap<String, KeyStroke>();
-		editItems.put("Undo", KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_DOWN_MASK));
-		editItems.put("Redo", KeyStroke.getKeyStroke(KeyEvent.VK_Y, InputEvent.CTRL_DOWN_MASK));
-		editItems.put("Cut", KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK));
-		editItems.put("Copy", KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
-		editItems.put("Paste", KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK));
-		editItems.put("Select All", KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_DOWN_MASK));
-		editItems.put("Delete", KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
-		editItems.put("Find", KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_DOWN_MASK));
-		editItems.put("Time and Date", KeyStroke.getKeyStroke(KeyEvent.VK_F5, 0));
+		undoItem = new JMenuItem("Undo");
+		undoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_DOWN_MASK));
+		undoItem.addActionListener(menuListener);
+		editMenu.add(undoItem);
 
-		for (String itemName : editItems.keySet()) {
-			JMenuItem item = new JMenuItem(itemName);
-			item.setAccelerator(editItems.get(itemName));
-			item.addActionListener(menuListener);
-			editMenu.add(item);
-		}
+		redoItem = new JMenuItem("Redo");
+		redoItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, InputEvent.CTRL_DOWN_MASK));
+		redoItem.addActionListener(menuListener);
+		editMenu.add(redoItem);
 
-		this.add(editMenu);
+		cutItem = new JMenuItem("Cut");
+		cutItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK));
+		cutItem.addActionListener(menuListener);
+		editMenu.add(cutItem);
+
+		copyItem = new JMenuItem("Copy");
+		copyItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+		copyItem.addActionListener(menuListener);
+		editMenu.add(copyItem);
+
+		pasteItem = new JMenuItem("Paste");
+		pasteItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK));
+		pasteItem.addActionListener(menuListener);
+		editMenu.add(pasteItem);
+
+		selectAllItem = new JMenuItem("Select All");
+		selectAllItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_DOWN_MASK));
+		selectAllItem.addActionListener(menuListener);
+		editMenu.add(selectAllItem);
+
+		deleteItem = new JMenuItem("Delete");
+		deleteItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
+		deleteItem.addActionListener(menuListener);
+		editMenu.add(deleteItem);
+
+		findItem = new JMenuItem("Find");
+		findItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_DOWN_MASK));
+		findItem.addActionListener(menuListener);
+		editMenu.add(findItem);
+
+		tDItem = new JMenuItem("Time and Date");
+		tDItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F5, 0));
+		tDItem.addActionListener(menuListener);
+		editMenu.add(tDItem);
+
+		add(editMenu);
 	}
 
 	private void addViewMenu() {
@@ -148,42 +224,55 @@ class MenuBar extends JMenuBar {
 		viewMenu.setForeground(menuItemTabForeground);
 		viewMenu.setMnemonic(KeyEvent.VK_V);
 
-		LinkedHashMap<String, KeyStroke> viewItems = new LinkedHashMap<String, KeyStroke>();
-		viewItems.put("Zoom In", KeyStroke.getKeyStroke(KeyEvent.VK_9, InputEvent.CTRL_DOWN_MASK));
-		viewItems.put("Zoom Out", KeyStroke.getKeyStroke(KeyEvent.VK_8, InputEvent.CTRL_DOWN_MASK));
-		viewItems.put("Reset Zoom", KeyStroke.getKeyStroke(KeyEvent.VK_0, InputEvent.CTRL_DOWN_MASK));
-		viewItems.put("Line Numbers", KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_DOWN_MASK));
-		viewItems.put("Toggle Theme", KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.CTRL_DOWN_MASK));
+		zoomInItem = new JMenuItem("Zoom In");
+		zoomInItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_9, InputEvent.CTRL_DOWN_MASK));
+		zoomInItem.addActionListener(menuListener);
+		viewMenu.add(zoomInItem);
 
-		for (String itemName : viewItems.keySet()) {
-			JMenuItem item = new JMenuItem(itemName);
-			item.setAccelerator(viewItems.get(itemName));
-			item.addActionListener(menuListener);
-			viewMenu.add(item);
-		}
+		zoomOutItem = new JMenuItem("Zoom Out");
+		zoomOutItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_8, InputEvent.CTRL_DOWN_MASK));
+		zoomOutItem.addActionListener(menuListener);
+		viewMenu.add(zoomOutItem);
 
-		this.add(viewMenu);
+		resetZoomItem = new JMenuItem("Reset Zoom");
+		resetZoomItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_0, InputEvent.CTRL_DOWN_MASK));
+		resetZoomItem.addActionListener(menuListener);
+		viewMenu.add(resetZoomItem);
+
+		lineNumbersItem = new JMenuItem("Line Numbers");
+		lineNumbersItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_DOWN_MASK));
+		lineNumbersItem.addActionListener(menuListener);
+		viewMenu.add(lineNumbersItem);
+
+		themeItem = new JMenuItem("Toggle Theme");
+		themeItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_T, InputEvent.CTRL_DOWN_MASK));
+		themeItem.addActionListener(menuListener);
+		viewMenu.add(themeItem);
+
+		add(viewMenu);
 	}
 
 	private void addHelpMenu() {
 		JMenu helpMenu = new JMenu("Help");
 		helpMenu.setForeground(menuItemTabForeground);
 		helpMenu.setMnemonic(KeyEvent.VK_H);
-		JMenuItem about = new JMenuItem("About");
-		about.addActionListener(menuListener);
-		helpMenu.add(about);
-		this.add(helpMenu);
+
+		aboutItem = new JMenuItem("About");
+		aboutItem.addActionListener(menuListener);
+		helpMenu.add(aboutItem);
+
+		add(helpMenu);
 	}
 
 	private void addFindBar() {
 		exitFindButton = new JButton("X");
 		exitFindButton.setVisible(false);
-		this.add(exitFindButton);
+		add(exitFindButton);
 
 		findField = new JTextField("");
 		findField.setVisible(false);
 		findField.addActionListener(menuListener);
-		this.add(findField);
+		add(findField);
 
 		addListenersToFindBar();
 	}
