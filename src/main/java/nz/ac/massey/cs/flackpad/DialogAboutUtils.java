f5bd5206cbeb89991b51e0048c9de5915dc785e9
Jack Sandford <jacksandford5757@gmail.com>
2021-09-01 00:57:38 +1200

    Finished PMD violation fixing, added CPD report

--- src/main/java/nz/ac/massey/cs/flackpad/DialogAboutUtils.java
+++ src/main/java/nz/ac/massey/cs/flackpad/DialogAboutUtils.java
@@ -16,65 +16,43 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-final class DialogAboutUtils {
+final class DialogAboutUtils extends MouseAdapter {
 	
+	//GitHub URLs
+	private static final String JACK_URL = "https://github.com/jcksndfrd";
+	private static final String FLETCH_URL = "https://github.com/fletchthefletch";
+	
+	// Colours
 	private static final Color LINK_COLOUR = Color.decode("#008787");
 	private static final Color HOVER_COLOUR = Color.decode("#009900");
 	
-	private DialogAboutUtils() {
-		throw new UnsupportedOperationException();
+	private final Component parent;
+	private final JLabel jack;
+	private final JLabel fletch;
+	
+	private DialogAboutUtils(Component parent) {
+		super();
+		
+		this.parent = parent;
+		
+	    // Jack's link
+	    jack = new JLabel("Jack Sandford");
+        jack.addMouseListener(this);
+	    jack.setForeground(LINK_COLOUR);
+        
+	    // Fletch's link
+	    fletch = new JLabel("Fletcher van Ameringen");
+	    fletch.addMouseListener(this);	 
+	    fletch.setForeground(LINK_COLOUR);
 	}
 	
 	static void show(Component parent, ImageIcon icon) {
+		final DialogAboutUtils links = new DialogAboutUtils(parent);
+		
 	    final JPanel jPanel = new JPanel();
 	    jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
 	    final JPanel jPanelFooting = new JPanel();
 	    
-	    // Jack's link
-	    final JLabel jack = new JLabel("Jack Sandford");
-        jack.addMouseListener(new MouseAdapter() {
-        	 
-            @Override
-            public void mouseClicked(MouseEvent event) {
-                try {
-                    Desktop.getDesktop().browse(new URI("https://github.com/jcksndfrd"));
-                } catch (IOException | URISyntaxException error) {
-        			DialogUtils.warning("Could not open URL", parent);
-                }
-            } 
-            @Override
-            public void mouseEntered(MouseEvent event) {
-            	jack.setForeground(HOVER_COLOUR);
-            } 
-            @Override
-            public void mouseExited(MouseEvent event) {
-            	jack.setForeground(LINK_COLOUR);
-            } 
-        });
-        
-	    // Fletch's link
-	    final JLabel fletch = new JLabel("Fletcher van Ameringen");
-	    fletch.addMouseListener(new MouseAdapter() {
-        	 
-            @Override
-            public void mouseClicked(MouseEvent event) {
-                try {
-                    Desktop.getDesktop().browse(new URI("https://github.com/fletchthefletch"));
-                } catch (IOException | URISyntaxException error) {
-        			DialogUtils.warning("Could not open URL", parent);
-                }
-            } 
-            @Override
-            public void mouseEntered(MouseEvent event) {
-                fletch.setForeground(HOVER_COLOUR);
-            } 
-            @Override
-            public void mouseExited(MouseEvent event) {
-                fletch.setForeground(LINK_COLOUR);
-            } 
-        });	 
-	    fletch.setForeground(LINK_COLOUR);
-	    jack.setForeground(LINK_COLOUR);
 	    
 	    final JLabel mainMessageA = new JLabel("This application was created for a tertiary assignment in late 2021."); 
 	    final JLabel mainMessageB = new JLabel("We hope you enjoy using it at much as we enjoyed making it!");
@@ -85,9 +63,9 @@ final class DialogAboutUtils {
 	    jPanel.setAlignmentY(SwingConstants.CENTER);
 	    
 	    jPanelFooting.add(new JLabel("-"));
-	    jPanelFooting.add(fletch);
+	    jPanelFooting.add(links.fletch);
 	    jPanelFooting.add(new JLabel("&"));
-	    jPanelFooting.add(jack);
+	    jPanelFooting.add(links.jack);
 	    jPanelFooting.setAlignmentX(SwingConstants.CENTER);
 	    jPanelFooting.setAlignmentY(SwingConstants.CENTER);
 	    
@@ -95,4 +73,27 @@ final class DialogAboutUtils {
 	    
 		JOptionPane.showMessageDialog(parent, jPanel, "About FlackPad", JOptionPane.INFORMATION_MESSAGE, icon);
 	}
+	
+	@Override
+    public void mouseClicked(MouseEvent event) {
+		final JLabel source = (JLabel) event.getSource();
+		
+        try {
+            Desktop.getDesktop().browse(new URI(source.equals(fletch) ? FLETCH_URL : JACK_URL));
+        } catch (IOException | URISyntaxException error) {
+			DialogUtils.warning("Could not open URL", parent);
+        }
+    }
+	
+    @Override
+    public void mouseEntered(MouseEvent event) {
+		final JLabel source = (JLabel) event.getSource();
+    	source.setForeground(HOVER_COLOUR);
+    }
+    
+    @Override
+    public void mouseExited(MouseEvent event) {
+		final JLabel source = (JLabel) event.getSource();
+    	source.setForeground(LINK_COLOUR);
+    } 
 }
