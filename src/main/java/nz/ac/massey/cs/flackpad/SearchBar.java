4900fa4acc0345244202d8853757d2a56cb6316a
fletchthefletch <fletcher@4tradeit.co.nz>
2021-09-01 00:08:12 +1200

    Fixed all searchbar violations

--- src/main/java/nz/ac/massey/cs/flackpad/SearchBar.java
+++ src/main/java/nz/ac/massey/cs/flackpad/SearchBar.java
@@ -10,30 +10,29 @@ import javax.swing.JMenuItem;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultHighlighter;
 import javax.swing.text.Highlighter;
 
 public class SearchBar {
-	private JMenuBar menu;
-	private JTextArea textArea;
+	final private JMenuBar menu;
+	final private JTextArea textArea;
 	private JButton findButton;
 	private JButton exitFindButton;
 	private JButton replaceButton;
-
-	private int warningBorderWidth = 1;
-	private String defaultFindText;
-	private String defaultReplaceText;
-
+	final static private int WARNING_WIDTH = 1;
+	private String defaultFText;
+	private String defaultRText;
 	private JTextField findField;
 	private JTextField replaceField;
-	private Border padding;
-	
-	private SearchBarListener searchBarListener;
+	final private Border padding;
+	final private SearchBarListener searchBarListener;
+	final static private Color BLUE = Color.decode("#000099");
 
 	public SearchBar(JTextArea textArea, JMenuBar menu) {
 		this.menu = menu;
 		this.textArea = textArea;
-		JMenuItem item = new JMenuItem();
+		final JMenuItem item = new JMenuItem();
 		item.removeMouseListener(item.getMouseListeners()[0]);
 		item.setFocusable(false);
 		item.setMargin(new Insets(2, 5, 2, 5));
@@ -51,12 +50,12 @@ public class SearchBar {
 	}
 
 	private void addReplaceBar() {
-		defaultReplaceText = "Replace with...";
+		defaultRText = "Replace with...";
 		replaceField = new JTextField("");
 		replaceField.setVisible(false);
-		replaceField.setBorder(BorderFactory.createLineBorder(Color.decode("#000099"), warningBorderWidth));
+		replaceField.setBorder(BorderFactory.createLineBorder(BLUE, WARNING_WIDTH));
 		replaceField.setForeground(Color.GRAY);
-		replaceField.setText(defaultReplaceText);
+		replaceField.setText(defaultRText);
 		replaceField.setBorder(BorderFactory.createCompoundBorder(replaceField.getBorder(), padding));
 
 		menu.add(replaceField);
@@ -72,14 +71,14 @@ public class SearchBar {
 		exitFindButton.setVisible(false);
 		menu.add(exitFindButton);
 
-		defaultFindText = "Find text...";
+		defaultFText = "Find text...";
 		findField = new JTextField("");
 		findField.setVisible(false);
-		findField.setBorder(BorderFactory.createLineBorder(Color.decode("#000099"), warningBorderWidth));
+		findField.setBorder(BorderFactory.createLineBorder(BLUE, WARNING_WIDTH));
 		findField.setBorder(BorderFactory.createCompoundBorder(findField.getBorder(), padding));
 
 		findField.setForeground(Color.GRAY);
-		findField.setText(defaultFindText);
+		findField.setText(defaultFText);
 		menu.add(findField);
 
 		findButton = new JButton("Find");
@@ -88,21 +87,17 @@ public class SearchBar {
 	}
 
 	void findText() {
-		try {
-			if (!checkFindWarnings()) {
-				return;
-			}
-
-			String searchtext = findField.getText();
-			String text = textArea.getText();
+		if (checkFindWarnings()) {
+			final String searchtext = findField.getText();
+			final String text = textArea.getText();
 
 			int offset = text.indexOf(searchtext);
-			int length = searchtext.length();
+			final int length = searchtext.length();
 
 			// Remove all current highlights
 			textArea.getHighlighter().removeAllHighlights();
 
-			Highlighter.HighlightPainter painter = new DefaultHighlighter.DefaultHighlightPainter(
+			final Highlighter.HighlightPainter painter = new DefaultHighlighter.DefaultHighlightPainter(
 					textArea.getSelectionColor());
 
 			// Get all occurrences
@@ -110,54 +105,51 @@ public class SearchBar {
 				try {
 					textArea.getHighlighter().addHighlight(offset, offset + length, painter);
 					offset = text.indexOf(searchtext, offset + 1);
-				} catch (Exception e1) {
+				} catch (BadLocationException error) {
         			DialogUtils.warning("Could not highlight search phrase", textArea);
 				}
 			}
-
-		} catch (Exception err) {
-			DialogUtils.warning("Could not find search phrase", textArea);
 		}
 	}
 
 	void replaceText() {
 		// If there is no text in the find bar or replace bar, do not replace anything
-		if (!checkReplaceWarnings() || !checkFindWarnings()) {
-			return;
-		}
-		try {
-			String searchtext = findField.getText();
-			String replacementtext = replaceField.getText();
-			String result = textArea.getText().replaceAll(searchtext, replacementtext);
+		if (checkReplaceWarnings() && checkFindWarnings()) {	
+			final String searchtext = findField.getText();
+			final String replacementtext = replaceField.getText();
+			final String result = textArea.getText().replaceAll(searchtext, replacementtext);
 			textArea.setText(result);
-		} catch (Exception e) {
-			DialogUtils.warning("Could not replace occurrences of text", textArea);
 		}
 	}
 
 	private boolean checkFindWarnings() {
-		if (findField.getText().equals(defaultFindText)) {
+		boolean checkWarning = false;
+		if (findField.getText().equals(defaultFText)) {
 			// Make field border red
-			findField.setBorder(BorderFactory.createLineBorder(Color.decode("#990000"), warningBorderWidth));
+			findField.setBorder(BorderFactory.createLineBorder(Color.decode("#990000"), WARNING_WIDTH));
 			findField.setBorder(BorderFactory.createCompoundBorder(findField.getBorder(), padding));
-
-			return false;
+		} else
+		{
+			findField.setBorder(BorderFactory.createLineBorder(BLUE, WARNING_WIDTH));
+			findField.setBorder(BorderFactory.createCompoundBorder(findField.getBorder(), padding));
+			checkWarning = true;
 		}
-		findField.setBorder(BorderFactory.createLineBorder(Color.decode("#000099"), warningBorderWidth));
-		findField.setBorder(BorderFactory.createCompoundBorder(findField.getBorder(), padding));
-		return true;
+		return checkWarning;
 	}
 
 	private boolean checkReplaceWarnings() {
-		if (replaceField.getText().equals(defaultReplaceText)) {
+		boolean checkWarning = false;
+		if (replaceField.getText().equals(defaultRText)) {
 			// Make field border red
-			replaceField.setBorder(BorderFactory.createLineBorder(Color.decode("#990000"), warningBorderWidth));
+			replaceField.setBorder(BorderFactory.createLineBorder(Color.decode("#990000"), WARNING_WIDTH));
 			replaceField.setBorder(BorderFactory.createCompoundBorder(replaceField.getBorder(), padding));
-			return false;
 		}
-		replaceField.setBorder(BorderFactory.createLineBorder(Color.decode("#000099"), warningBorderWidth));
-		replaceField.setBorder(BorderFactory.createCompoundBorder(replaceField.getBorder(), padding));
-		return true;
+		else {
+			replaceField.setBorder(BorderFactory.createLineBorder(BLUE, WARNING_WIDTH));
+			replaceField.setBorder(BorderFactory.createCompoundBorder(replaceField.getBorder(), padding));
+			checkWarning = true;
+		}
+		return checkWarning;
 	}
 
 	private void addListenersToSearchBar() {
@@ -177,19 +169,19 @@ public class SearchBar {
 		replaceButton.addActionListener(searchBarListener);
 	}
 	void setFindField() {
-		if (findField.getText().equals(defaultFindText)) {
+		if (findField.getText().equals(defaultFText)) {
 			findField.setText("");
 			findField.setForeground(Color.BLACK);
 		}
 	}
 	void resetFindField() {
 		if (findField.getText().isEmpty()) {
-			findField.setText(defaultFindText);
+			findField.setText(defaultFText);
 			findField.setForeground(Color.GRAY);
 		}
 	}
 	void setReplaceField() {
-		if (replaceField.getText().equals(defaultReplaceText)) {
+		if (replaceField.getText().equals(defaultRText)) {
 			replaceField.setText("");
 			replaceField.setForeground(Color.BLACK);
 		}
@@ -197,35 +189,26 @@ public class SearchBar {
 	void resetReplaceField() {
 		if (replaceField.getText().isEmpty()) {
 			replaceField.setForeground(Color.GRAY);
-			replaceField.setText(defaultReplaceText);
+			replaceField.setText(defaultRText);
 		}
 	}
 
 	void hideFindBar() {
-		try {
-			findField.setVisible(false);
-			findButton.setVisible(false);
-			exitFindButton.setVisible(false);
-			replaceButton.setVisible(false);
-			replaceField.setVisible(false);
-			textArea.getHighlighter().removeAllHighlights();
-		} catch (Exception e) {
-			DialogUtils.warning("Could not hide search bar", textArea);
-		}
-
+		findField.setVisible(false);
+		findButton.setVisible(false);
+		exitFindButton.setVisible(false);
+		replaceButton.setVisible(false);
+		replaceField.setVisible(false);
+		textArea.getHighlighter().removeAllHighlights();
 	}
 
 	void showFindBar() {
-		try {
-			findField.setVisible(true);
-			findButton.setVisible(true);
-			exitFindButton.setVisible(true);
-			findField.requestFocus();
-			replaceButton.setVisible(true);
-			replaceField.setVisible(true);
-		} catch (Exception e) {
-			DialogUtils.warning("Could not show search bar", textArea);
-		}
+		findField.setVisible(true);
+		findButton.setVisible(true);
+		exitFindButton.setVisible(true);
+		findField.requestFocus();
+		replaceButton.setVisible(true);
+		replaceField.setVisible(true);
 	}
 
 	JTextField getFindField() {
